4d447977628be8045614233febbe22cad0ed2b86
Natasha Kerensikova <natacha@instinctive.eu>
2015-09-07 16:56:56 +0000

    Add infrastructure files

---
+++ README.md
@@ -0,0 +1,11 @@
+`lithium` is the name I give to the CMS-ish web application that powers my
+personal website, currently at <http://instinctive.eu/>. This is the third
+rewrite from scratch, hence the name `lithium3`.
+
+Its mostly a straightforward use of the components I gathered in the library
+called `natools-web`. It also depends on my projects `ada-syslog` for logging
+and `markup-ada` for Markdown rendering.
+
+Looking at it from the other way around, `lithium3` is probably of the
+simplest possible real-world example application of `natools-web` and also
+of `markup-ada` taken as an Ada library.
---
+++ src/lithium.ads
@@ -0,0 +1,25 @@
+------------------------------------------------------------------------------
+-- Copyright (c) 2015, Natacha Porté                                        --
+--                                                                          --
+-- Permission to use, copy, modify, and distribute this software for any    --
+-- purpose with or without fee is hereby granted, provided that the above   --
+-- copyright notice and this permission notice appear in all copies.        --
+--                                                                          --
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES --
+-- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF         --
+-- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR  --
+-- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES   --
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN    --
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF  --
+-- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.           --
+------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
+-- Lithium is an empty parent package for all code specific to the lithium  --
+-- web engine.                                                              --
+------------------------------------------------------------------------------
+
+package Lithium is
+   pragma Pure;
+
+end Lithium;
