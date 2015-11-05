7f4e49b26ca6230202af1fe580649fb5861e44c9
Natasha Kerensikova <natacha@instinctive.eu>
2015-11-05 18:06:30 +0000

    sqlite3: thin bindings for SQLite, by Tero Koskinen
    Taken from the repository at https://bitbucket.org/tkoskine/sqlite3-ada

---
+++ src/sqlite3.adb
@@ -0,0 +1,230 @@
+--
+-- Copyright (c) 2009, 2012 Tero Koskinen <tero.koskinen@iki.fi>
+--
+-- Permission to use, copy, modify, and distribute this software for any
+-- purpose with or without fee is hereby granted, provided that the above
+-- copyright notice and this permission notice appear in all copies.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+-- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+-- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+-- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+--
+with Interfaces.C;
+with Interfaces.C.Strings;
+
+package body SQLite3 is
+   procedure Open (Filename : in String;
+                   Handle   : out SQLite3_DB;
+                   Status   : out Error_Code)
+   is
+      function sqlite3_open (Filename : Interfaces.C.Char_Array;
+                             DB_Handle : access DB_Private_Access)
+         return Interfaces.C.Int;
+      pragma Import (C, sqlite3_open, "sqlite3_open");
+
+      Code : Interfaces.C.Int;
+   begin
+      Code := sqlite3_open (Interfaces.C.To_C (Filename), Handle.Ptr'Access);
+      Status := Error_Code (Code);
+   end Open;
+
+   procedure Close (Handle : in out SQLite3_DB; Status : out Error_Code) is
+      function sqlite3_close (DB_Handle : DB_Private_Access)
+         return Interfaces.C.Int;
+      pragma Import (C, sqlite3_close, "sqlite3_close");
+
+      Code : Interfaces.C.Int;
+   begin
+      Code := sqlite3_close (Handle.Ptr);
+      Status := Error_Code (Code);
+   end Close;
+
+   procedure Prepare (Handle     : SQLite3_DB;
+                      Sql        : String;
+                      SQL_Handle : out SQLite3_Statement;
+                      Status     : out Error_Code) is
+      type Chars_Ptr_Ptr is access all Interfaces.C.Strings.Chars_Ptr;
+
+      -- int sqlite3_prepare_v2(
+      --   sqlite3 *db,            /* Database handle */
+      --   const char *zSql,       /* SQL statement, UTF-8 encoded */
+      --   int nByte,              /* Maximum length of zSql in bytes. */
+      --   sqlite3_stmt **ppStmt,  /* OUT: Statement handle */
+      --   const char **pzTail     /* OUT: Pointer to unused portion of zSql */
+      -- );
+      function sqlite3_prepare_v2
+        (DB_Handle : DB_Private_Access;
+         zSql      : Interfaces.C.Char_Array;
+         nByte     : Interfaces.C.Int;
+         ppStmt    : access Statement_Private_Access;
+         pzTail    : Chars_Ptr_Ptr)
+        return Interfaces.C.Int;
+      pragma Import (C, sqlite3_prepare_v2, "sqlite3_prepare_v2");
+
+      Sql_Str : Interfaces.C.Char_Array :=
+        Interfaces.C.To_C (Sql);
+      Code : Interfaces.C.Int;
+   begin
+      Code := sqlite3_prepare_v2
+        (DB_Handle => Handle.Ptr,
+         zSql      => Sql_Str,
+         nByte     => Sql_Str'Length,
+         ppStmt    => Sql_Handle.Ptr'Access,
+         pzTail    => null);
+      Status := Error_Code (Code);
+   end Prepare;
+
+   procedure Step (SQL_Handle : SQLite3_Statement; Status : out Error_Code) is
+      -- int sqlite3_step(sqlite3_stmt*);
+      function sqlite3_step (Stmt : Statement_Private_Access)
+        return Interfaces.C.Int;
+      pragma Import (C, sqlite3_step, "sqlite3_step");
+
+      Code : Interfaces.C.Int;
+   begin
+      Code := sqlite3_step (SQL_Handle.Ptr);
+      Status := Error_Code (Code);
+   end Step;
+
+   procedure Finish (SQL_Handle : SQLite3_Statement;
+                     Status     : out Error_Code) is
+      -- int sqlite3_finalize(sqlite3_stmt*);
+      function sqlite3_finalize (Stmt : Statement_Private_Access)
+        return Interfaces.C.Int;
+      pragma Import (C, sqlite3_finalize, "sqlite3_finalize");
+
+      Code : Interfaces.C.Int;
+   begin
+      Code := sqlite3_finalize (SQL_Handle.Ptr);
+      Status := Error_Code (Code);
+   end Finish;
+
+   procedure Reset (SQL_Handle : SQLite3_Statement;
+                    Status     : out Error_Code) is
+      -- int sqlite3_reset(sqlite3_stmt*);
+      function sqlite3_reset (Stmt : Statement_Private_Access)
+        return Interfaces.C.Int;
+      pragma Import (C, sqlite3_reset, "sqlite3_reset");
+
+      Code : Interfaces.C.Int;
+   begin
+      Code := sqlite3_reset (SQL_Handle.Ptr);
+      Status := Error_Code (Code);
+   end Reset;
+
+   procedure Bind (SQL_Handle : SQLite3_Statement;
+                   Index      : SQL_Parameter_Index;
+                   Value      : Integer;
+                   Status     : out Error_Code) is
+   begin
+      Bind (SQL_Handle, Index, Long_Integer (Value), Status);
+   end Bind;
+
+   procedure Bind (SQL_Handle : SQLite3_Statement;
+                   Index      : SQL_Parameter_Index;
+                   Value      : Long_Integer;
+                   Status     : out Error_Code) is
+      -- int sqlite3_bind_int(sqlite3_stmt*, int, int);
+      function sqlite3_bind_int (Stmt  : Statement_Private_Access;
+                                 Index : Interfaces.C.Int;
+                                 Value : Interfaces.C.Int)
+        return Interfaces.C.Int;
+      pragma Import (C, sqlite3_bind_int, "sqlite3_bind_int");
+
+      Code : Interfaces.C.Int;
+   begin
+      Code := sqlite3_bind_int (Stmt  => Sql_Handle.Ptr,
+                                Index => Interfaces.C.Int (Index),
+                                Value => Interfaces.C.Int (Value));
+      Status := Error_Code (Code);
+   end Bind;
+
+   procedure Bind (SQL_Handle : SQLite3_Statement;
+                   Index      : SQL_Parameter_Index;
+                   Value      : Ada.Strings.Unbounded.Unbounded_String;
+                   Status     : out Error_Code)
+   is
+
+      use Ada.Strings.Unbounded;
+
+      -- int sqlite3_bind_text
+      --       (sqlite3_stmt*,
+      --        int,
+      --        const char*,
+      --        int,
+      --        void(*)(void*));
+      function sqlite3_bind_text (Stmt  : Statement_Private_Access;
+                                  Index : Interfaces.C.int;
+                                  Value : Interfaces.C.char_array;
+                                  Bytes : Interfaces.C.int;
+                                  Opt   : Interfaces.C.long)
+        return Interfaces.C.Int;
+      pragma Import (C, sqlite3_bind_text, "sqlite3_bind_text");
+
+      Code : Interfaces.C.Int;
+   begin
+      Code := sqlite3_bind_text
+        (Stmt  => Sql_Handle.Ptr,
+         Index => Interfaces.C.int (Index),
+         Value => Interfaces.C.To_C (To_String (Value)),
+         Bytes => Interfaces.C.int (Length (Value)),
+         Opt   => SQLITE_TRANSIENT);
+      Status := Error_Code (Code);
+   end Bind;
+
+   procedure Column (SQL_Handle : SQLite3_Statement;
+                     Index      : SQL_Column_Index;
+                     Value      : out Int) is
+      -- int sqlite3_column_int(sqlite3_stmt*, int iCol);
+      function sqlite3_column_int (Stmt  : Statement_Private_Access;
+                                   Index : Interfaces.C.Int)
+        return Interfaces.C.Int;
+      pragma Import (C, sqlite3_column_int, "sqlite3_column_int");
+
+      Ret_Val : Interfaces.C.Int;
+   begin
+      Ret_Val := sqlite3_column_int (Stmt => Sql_Handle.Ptr,
+                                     Index => Interfaces.C.Int (Index));
+      Value := Int (Ret_Val);
+   end Column;
+
+   procedure Column (SQL_Handle : SQLite3_Statement;
+                     Index      : SQL_Column_Index;
+                     Value      : out Ada.Strings.Unbounded.Unbounded_String)
+   is
+      use Ada.Strings.Unbounded;
+      use Interfaces;
+      use type Interfaces.C.Strings.chars_ptr;
+
+      -- const unsigned char *sqlite3_column_text(sqlite3_stmt*, int iCol);
+      function sqlite3_column_text (Stmt  : Statement_Private_Access;
+                                    Index : Interfaces.C.Int)
+        return Interfaces.C.Strings.chars_ptr;
+      pragma Import (C, sqlite3_column_text, "sqlite3_column_text");
+
+      Ret_Val : Interfaces.C.Strings.chars_ptr;
+   begin
+      Ret_Val := sqlite3_column_text (Stmt => Sql_Handle.Ptr,
+                                      Index => Interfaces.C.Int (Index));
+      if Ret_Val = C.Strings.Null_Ptr then
+         Value := Null_Unbounded_String;
+      else
+         Value := To_Unbounded_String (C.To_Ada (C.Strings.Value (Ret_Val)));
+      end if;
+   end Column;
+
+   function Error_Message (Handle : SQLite3_DB) return String is
+      use Interfaces;
+
+      -- const char *sqlite3_errmsg(sqlite3*);
+      function sqlite3_errmsg(DB_Handle : DB_Private_Access)
+        return C.Strings.chars_ptr;
+      pragma Import (C, sqlite3_errmsg, "sqlite3_errmsg");
+   begin
+      return C.To_Ada (C.Strings.Value (sqlite3_errmsg (Handle.Ptr)));
+   end Error_Message;
+end SQLite3;
---
+++ src/sqlite3.ads
@@ -0,0 +1,89 @@
+--
+-- Copyright (c) 2009, 2012 Tero Koskinen <tero.koskinen@iki.fi>
+--
+-- Permission to use, copy, modify, and distribute this software for any
+-- purpose with or without fee is hereby granted, provided that the above
+-- copyright notice and this permission notice appear in all copies.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+-- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+-- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+-- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+--
+with Ada.Strings.Unbounded;
+with System;
+with Interfaces.C;
+
+package SQLite3 is
+   type SQLite3_DB is private;
+   type SQLite3_Statement is private;
+   type Error_Code is new Long_Integer;
+   subtype SQL_Parameter_Index is Integer range 1 .. Integer'Last;
+   subtype SQL_Column_Index is Integer;
+   type Int is new Interfaces.C.Int;
+
+   SQLITE_OK    : constant Error_Code := 0;
+   SQLITE_ERROR : constant Error_Code := 1;
+   SQLITE_ROW   : constant Error_Code := 100;
+   SQLITE_DONE  : constant Error_Code := 101;
+
+   SQLITE_TRANSIENT : constant := -1;
+
+   procedure Open (Filename : in String;
+                   Handle   : out SQLite3_DB;
+                   Status   : out Error_Code);
+
+   procedure Close (Handle : in out SQLite3_DB; Status : out Error_Code);
+
+   procedure Prepare (Handle     : SQLite3_DB;
+                      Sql        : String;
+                      SQL_Handle : out SQLite3_Statement;
+                      Status     : out Error_Code);
+
+   procedure Step (SQL_Handle : SQLite3_Statement; Status : out Error_Code);
+
+   procedure Finish (SQL_Handle : SQLite3_Statement; Status : out Error_Code);
+
+   procedure Reset (SQL_Handle : SQLite3_Statement; Status : out Error_Code);
+
+   procedure Bind (SQL_Handle : SQLite3_Statement;
+                   Index      : SQL_Parameter_Index;
+                   Value      : Integer;
+                   Status     : out Error_Code);
+
+   procedure Bind (SQL_Handle : SQLite3_Statement;
+                   Index      : SQL_Parameter_Index;
+                   Value      : Long_Integer;
+                   Status     : out Error_Code);
+
+   procedure Bind (SQL_Handle : SQLite3_Statement;
+                   Index      : SQL_Parameter_Index;
+                   Value      : Ada.Strings.Unbounded.Unbounded_String;
+                   Status     : out Error_Code);
+
+   procedure Column (SQL_Handle : SQLite3_Statement;
+                     Index      : SQL_Column_Index;
+                     Value      : out Int);
+
+   procedure Column (SQL_Handle : SQLite3_Statement;
+                     Index      : SQL_Column_Index;
+                     Value      : out Ada.Strings.Unbounded.Unbounded_String);
+
+   function Error_Message (Handle : SQLite3_DB) return String;
+private
+   type DB_Private is null record;
+   type DB_Private_Access is access all DB_Private;
+
+   type SQLite3_DB is record
+      Ptr : aliased DB_Private_Access;
+   end record;
+
+   type Statement_Private is null record;
+   type Statement_Private_Access is access all Statement_Private;
+   type SQLite3_Statement is record
+      Ptr : aliased Statement_Private_Access;
+   end record;
+end SQLite3;
